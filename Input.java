bcc44bac3cf61f33f029d7112977752804a42a15
imtiazashraf1 <imtiazashraf2004@gmail.com>
2025-07-18 21:03:08 +0600

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,18 @@
+package imtiazashraf;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int a = Sc.nextInt();
+        int b = Sc.nextInt();
+        int sum = a + b;
+        int diff = a - b;
+        double div = a / b;
+        int mul = a * b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(div);
+        System.out.print(mul);
+    }
+}
